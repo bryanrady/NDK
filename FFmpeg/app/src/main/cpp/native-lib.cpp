6c9e0504280c06698d56b59dfa263d128cf335ab
bryanrady <bryanrady@163.com>
2019-11-08 16:20:55 +0800

    将编译好的FFmpeg静态库引入到工程，并且配置好开发环境,现在可以进行开发了;

--- FFmpeg/app/src/main/cpp/native-lib.cpp
+++ FFmpeg/app/src/main/cpp/native-lib.cpp
@@ -1,13 +1,19 @@
 #include <jni.h>
 #include <string>
+#include <android/log.h>
+#include "safe_queue.h"
+#define  LOGE(...) __android_log_print(ANDROID_LOG_ERROR,"JNI",__VA_ARGS__);    //  __VA_ARGS__ 代表... 可变参数
+
+extern "C"{
+#include "include/libavutil/avutil.h"
+}
 
 extern "C"
-JNIEXPORT jstring
-
-JNICALL
+JNIEXPORT jstring JNICALL
 Java_com_bryanrady_ffmpeg_MainActivity_stringFromJNI(
         JNIEnv *env,
         jobject /* this */) {
     std::string hello = "Hello from C++";
-    return env->NewStringUTF(hello.c_str());
+    char *versionInfo = (char*)av_version_info();
+    return env->NewStringUTF(versionInfo);
 }
