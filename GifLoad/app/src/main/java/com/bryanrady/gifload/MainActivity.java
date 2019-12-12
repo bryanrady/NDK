a1d934f48456838f2e36e14e94bd509788dc983d
bryanrady <bryanrady@163.com>
2019-12-12 20:43:06 +0800

    Gif:native层实现加载gif动画图片。

--- GifLoad/app/src/main/java/com/bryanrady/gifload/MainActivity.java
+++ GifLoad/app/src/main/java/com/bryanrady/gifload/MainActivity.java
@@ -23,16 +23,17 @@ import static android.content.pm.PackageManager.PERMISSION_GRANTED;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView mImageView;
-    private Bitmap mBitmap;
-    private GifHandler mGifHandler;
+    private ImageView mImageView1,mImageView2;
+    private Bitmap mBitmap1,mBitmap2;
+    private GifHandler mGifHandler1,mGifHandler2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mImageView = findViewById(R.id.imageView);
+        mImageView1 = findViewById(R.id.imageView1);
+        mImageView2 = findViewById(R.id.imageView2);
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {//如果 API level 是大于等于 23(Android 6.0) 时
             //判断是否具有权限
@@ -43,18 +44,28 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        ndkLoadGif();
-
+        ndkLoadGif1();
+        ndkLoadGif2();
     }
 
     //https://blog.csdn.net/a756213932/article/details/101853198    android ndk使用giflib高性能显示gif
-    public void ndkLoadGif() {
+    public void ndkLoadGif1() {
+        File file = new File(Environment.getExternalStorageDirectory(),"demo.gif");
+        if(file.exists()){
+            mGifHandler1 = new GifHandler(file.getAbsolutePath());
+            mBitmap1 = Bitmap.createBitmap(mGifHandler1.getWidth(),mGifHandler1.getHeight(), Bitmap.Config.ARGB_8888);
+            int delay = mGifHandler1.updateFrame(mBitmap1);
+            handler.sendEmptyMessageDelayed(1,delay);
+        }
+    }
+
+    public void ndkLoadGif2() {
         File file = new File(Environment.getExternalStorageDirectory(),"demo2.gif");
         if(file.exists()){
-            mGifHandler = new GifHandler(file.getAbsolutePath());
-            mBitmap = Bitmap.createBitmap(mGifHandler.getWidth(),mGifHandler.getHeight(), Bitmap.Config.ARGB_8888);
-            int delay = mGifHandler.updateFrame(mBitmap);
-            handler.sendEmptyMessageDelayed(1,delay);
+            mGifHandler2 = new GifHandler(file.getAbsolutePath());
+            mBitmap2 = Bitmap.createBitmap(mGifHandler2.getWidth(),mGifHandler2.getHeight(), Bitmap.Config.ARGB_8888);
+            int delay = mGifHandler2.updateFrame(mBitmap2);
+            handler.sendEmptyMessageDelayed(2,delay);
         }
     }
 
@@ -64,9 +75,14 @@ public class MainActivity extends AppCompatActivity {
         public void handleMessage(@NonNull Message msg) {
             switch (msg.what){
                 case 1:
-                    int delay = mGifHandler.updateFrame(mBitmap);
-                    mImageView.setImageBitmap(mBitmap);
+                    int delay = mGifHandler1.updateFrame(mBitmap1);
+                    mImageView1.setImageBitmap(mBitmap1);
                     handler.sendEmptyMessageDelayed(1,delay);
+                    break;
+                case 2:
+                    int delay2 = mGifHandler2.updateFrame(mBitmap2);
+                    mImageView2.setImageBitmap(mBitmap2);
+                    handler.sendEmptyMessageDelayed(2,delay2);
                     break;
             }
         }
@@ -76,6 +92,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        mGifHandler.release();
+        mGifHandler1.release();
+        mGifHandler2.release();
     }
 }
